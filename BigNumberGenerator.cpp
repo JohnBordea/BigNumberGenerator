e0457676fdb7bd9713d9c37e57d0daa64b6599ef
Ion <Ion@DESKTOP-RIIT7BR>
2021-03-17 15:30:13 +0200

    Inplemet Main Menu

--- BigNumberGenerator.cpp
+++ BigNumberGenerator.cpp
@@ -6,10 +6,50 @@ using namespace NTL;
 
 int main()
 {
-	ZZ a, b, c;
+	int choiceMainMenu, choiceBBS, choiceJacobi;
 
-	cin >> a;
-	cin >> b;
-	c = a * b;
-	cout << c << "\n";
+	choiceMainMenu = 0;
+
+	while (choiceMainMenu != -1) {
+		switch (choiceMainMenu) {
+		case 0:
+			cout << "What generator do you want to use?\n1. B.B.S. Generator\n2. Jacobi Generator\n3. Exit\n";
+			do {
+				cin >> choiceMainMenu;
+			} while (choiceMainMenu <= 0 || choiceMainMenu > 3);
+			if (choiceMainMenu == 3)
+				choiceMainMenu = -1;
+			break;
+		case 1:
+			cout << "BBS Generator\n";
+			choiceMainMenu = 0;
+			break;
+		case 2:
+			cout << "Jacobi Generator\n";
+			choiceMainMenu = 0;
+			break;
+		}
+	}
+
+	/*c = 0;
+	a = RandomBits_ZZ(512);
+	b = a;
+	while (b != 0) {
+		f[c] = b % 2;
+		b = (b >> 1);
+		c++;
+	}
+
+	b = 0;
+	for (int i = c - 1; i > 0; i--) {
+		b += f[i];
+		b = (b << 1);
+	}
+	b += f[0];
+
+	//c = a * b;
+	cout << a << "\n" << c << " " << NumBits(a) << "\n" << b << "\n";
+
+	a = GenPrime_ZZ(512);
+	cout << a << "\n" << ProbPrime(a);*/
 }
