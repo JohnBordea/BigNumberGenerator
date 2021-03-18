707412afc6256e020da276796fc5b0e334f0c250
Ion <Ion@DESKTOP-RIIT7BR>
2021-03-18 18:10:10 +0200

    Implemented Jacobi Symbol, just started.

--- BigNumberGenerator.cpp
+++ BigNumberGenerator.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <NTL/ZZ.h>
+//#include <zlib.h>
 
 using namespace std;
 using namespace NTL;
@@ -33,12 +34,63 @@ ZZ generateNumberBBS(unsigned long bitLength) {
 	return randomNumber;
 }
 
-void testPercentageBBS(int iterations) {
+ZZ jacobiSymbol(ZZ n, ZZ k) {
+	n %= k;
+	int t = 1;
+	while (n != 0) {
+		while (n % 2 == 0) {
+			n /= 2;
+			int r = k % 8;
+			if (r == 3 || r == 5)
+				t = -t;
+		}
+		swap(n, k);
+		if (n % 4 == 3 && k % 4 == 3)
+			t = -t;
+		n %= k;
+	}
+	return (k == 1) ? (ZZ)t : (ZZ)0;
+}
+
+ZZ jacobiSimbol(ZZ a, ZZ n) {
+	ZZ b = a % n;
+	ZZ swp;
+	ZZ c = n;
+	int s = 1;
+
+	while (b > 2) {
+		while (b % 4 == 0)
+			b /= 4;
+		if (b % 2 == 0) {
+			if (c % 8 == 3 || c % 8 == 5)
+				s = -s;
+			b /= 2;
+		}
+		if (b == 1)
+			break;
+		if (b % 4 == 3 && c % 4 == 3) {
+			s = -s;
+			swp = c;
+			c = b;
+			b = swp % b;
+		}
+	}
+
+	return b * s;
+}
+
+void testPercentage(int iterations, int type) {
 	int countZero = 0, countOne = 0, count;
 	ZZ number;
 
 	for (int i = 0; i < iterations; i++) {
-		number = generateNumberBBS(1024);
+		switch (type) {
+		case 1:
+			number = generateNumberBBS(1024);
+			break;
+		default:
+			number = generateNumberBBS(1024);
+		}
 		while (number != 0) {
 			switch (bit(number, 0) /*number % 2*/) {
 			case 0:
@@ -63,9 +115,9 @@ int main()
 {
 	int choiceMainMenu, choiceBBS, choiceJacobi;
 	unsigned long numberLength = 1024;
-	ZZ generatedNumberBBS = (ZZ)0;
+	ZZ generatedNumber = (ZZ)0;
 
-	choiceMainMenu = 1;
+	choiceMainMenu = -1;
 
 	while (choiceMainMenu != -1) {
 		switch (choiceMainMenu) {
@@ -90,7 +142,7 @@ int main()
 				cout << generateNumberBBS(numberLength) << "\n";
 				break;
 			case 2:
-				testPercentageBBS(20);
+				testPercentage(20, 1);
 				break;
 			case 3:
 				break;
@@ -101,10 +153,39 @@ int main()
 
 			break;
 		case 2:
-			cout << "Jacobi Generator\n";
-			choiceMainMenu = 0;
+			cout << "Jacobi Generator\n1. Generate a number\n2. Test the Generator(Percentage of Bits)\n3. Test the Generator(Compresion)\n4. Back\n";
+			do {
+				cin >> choiceJacobi;
+			} while (choiceJacobi <= 0 || choiceJacobi > 4);
+
+			switch (choiceJacobi) {
+			case 1:
+				
+				break;
+			case 2:
+				testPercentage(20, 1);
+				break;
+			case 3:
+				break;
+			case 4:
+				choiceMainMenu = 0;
+				break;
+			}
 			break;
 		}
+	}
+
+	cout << 1;
+
+	ZZ a, b;
+
+	b = GenPrime_ZZ(512);
+	a = RandomBnd(b);
+
+	for (int i = 0; i < 100; i++) {
+		b = GenPrime_ZZ(512);
+		a = RandomBnd(b);
+		cout << "[" << jacobiSymbol(a, b) << " " << Jacobi(a, b) << "]";
 	}
 
 	/*c = 0;
