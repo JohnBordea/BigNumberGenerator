fdb53d604ea1e5e9ef20a571995d43781cfea57d
Ion <Ion@DESKTOP-RIIT7BR>
2021-03-18 21:34:30 +0200

    Implemented Jacobi Sequence generator. The implementation of Test by Compresion had to be canceled, due to the need of using an external library. Needs to be done.

--- BigNumberGenerator.cpp
+++ BigNumberGenerator.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <NTL/ZZ.h>
-//#include <zlib.h>
 
 using namespace std;
 using namespace NTL;
@@ -10,13 +9,31 @@ ZZ generateNumberBBS(unsigned long bitLength) {
 	ZZ p, q;
 	do {
 		do {
-			p = GenPrime_ZZ(512);
+			p = GenPrime_ZZ(bitLength / 2);
 		} while (p % 4 != 3);
 		do {
-			q = GenPrime_ZZ(512);
+			q = GenPrime_ZZ(bitLength / 2);
 		} while (q % 4 != 3);
 	} while (p == q);
 
+	ZZ N = p * q;
+
+	ZZ seed;
+	do {
+		seed = RandomBnd(N);
+	} while (seed % p == 0 || seed % q == 0);
+
+	ZZ randomNumber = (ZZ)0;
+
+	for (int i = 0; i < bitLength; i++) {
+		seed = (seed * seed) % N;
+		randomNumber = (randomNumber << 1) + (seed % 2);
+	}
+
+	return randomNumber;
+}
+
+ZZ generateNumberBBS(ZZ p, ZZ q, unsigned long bitLength) {
 	ZZ N = p * q;
 
 	ZZ seed;
@@ -52,41 +69,40 @@ ZZ jacobiSymbol(ZZ n, ZZ k) {
 	return (k == 1) ? (ZZ)t : (ZZ)0;
 }
 
-ZZ jacobiSimbol(ZZ a, ZZ n) {
-	ZZ b = a % n;
-	ZZ swp;
-	ZZ c = n;
-	int s = 1;
+ZZ generateNumberJacobi(unsigned long bitLength) {
+	ZZ a, n;
 
-	while (b > 2) {
-		while (b % 4 == 0)
-			b /= 4;
-		if (b % 2 == 0) {
-			if (c % 8 == 3 || c % 8 == 5)
-				s = -s;
-			b /= 2;
-		}
-		if (b == 1)
-			break;
-		if (b % 4 == 3 && c % 4 == 3) {
-			s = -s;
-			swp = c;
-			c = b;
-			b = swp % b;
-		}
+	n = GenPrime_ZZ(bitLength);
+	a = RandomBnd(n);
+
+	ZZ randomNumber = (ZZ)0;
+
+	for (int i = 0; i < bitLength; i++) {
+		randomNumber <<= 1;
+		if (jacobiSymbol(a + i, n) != (ZZ)-1)
+			randomNumber += 1;
 	}
 
-	return b * s;
+	return randomNumber;
 }
 
 void testPercentage(int iterations, int type) {
 	int countZero = 0, countOne = 0, count;
 	ZZ number;
+	ZZ p, q, n;
 
 	for (int i = 0; i < iterations; i++) {
 		switch (type) {
 		case 1:
-			number = generateNumberBBS(1024);
+			do {
+				do {
+					p = GenPrime_ZZ(512);
+				} while (p % 4 != 3);
+				do {
+					q = GenPrime_ZZ(512);
+				} while (q % 4 != 3);
+			} while (p == q);
+			number = generateNumberBBS(p, q, 1024);
 			break;
 		default:
 			number = generateNumberBBS(1024);
@@ -117,7 +133,7 @@ int main()
 	unsigned long numberLength = 1024;
 	ZZ generatedNumber = (ZZ)0;
 
-	choiceMainMenu = -1;
+	choiceMainMenu = 0;
 
 	while (choiceMainMenu != -1) {
 		switch (choiceMainMenu) {
@@ -130,10 +146,10 @@ int main()
 				choiceMainMenu = -1;
 			break;
 		case 1:
-			cout << "BBS Generator\n1. Generate a number\n2. Test the Generator(Percentage of Bits)\n3. Test the Generator(Compresion)\n4. Back\n";
+			cout << "BBS Generator\n1. Generate a number\n2. Test the Generator(Percentage of Bits)\n3. Back\n";
 			do {
 				cin >> choiceBBS;
-			} while (choiceBBS <= 0 || choiceBBS > 4);
+			} while (choiceBBS <= 0 || choiceBBS > 3);
 
 			switch (choiceBBS) {
 			case 1:
@@ -145,68 +161,31 @@ int main()
 				testPercentage(20, 1);
 				break;
 			case 3:
-				break;
-			case 4:
 				choiceMainMenu = 0;
 				break;
 			}
 
 			break;
 		case 2:
-			cout << "Jacobi Generator\n1. Generate a number\n2. Test the Generator(Percentage of Bits)\n3. Test the Generator(Compresion)\n4. Back\n";
+			cout << "Jacobi Generator\n1. Generate a number\n2. Test the Generator(Percentage of Bits)\n3. Back\n";
 			do {
 				cin >> choiceJacobi;
-			} while (choiceJacobi <= 0 || choiceJacobi > 4);
+			} while (choiceJacobi <= 0 || choiceJacobi > 3);
 
 			switch (choiceJacobi) {
 			case 1:
-				
+				cout << "Input Number's Bit Length\n";
+				cin >> numberLength;
+				cout << generateNumberJacobi(numberLength) << "\n";
 				break;
 			case 2:
-				testPercentage(20, 1);
+				testPercentage(10, 2);
 				break;
 			case 3:
-				break;
-			case 4:
 				choiceMainMenu = 0;
 				break;
 			}
 			break;
 		}
 	}
-
-	cout << 1;
-
-	ZZ a, b;
-
-	b = GenPrime_ZZ(512);
-	a = RandomBnd(b);
-
-	for (int i = 0; i < 100; i++) {
-		b = GenPrime_ZZ(512);
-		a = RandomBnd(b);
-		cout << "[" << jacobiSymbol(a, b) << " " << Jacobi(a, b) << "]";
-	}
-
-	/*c = 0;
-	a = RandomBits_ZZ(512);
-	b = a;
-	while (b != 0) {
-		f[c] = b % 2;
-		b = (b >> 1);
-		c++;
-	}
-
-	b = 0;
-	for (int i = c - 1; i > 0; i--) {
-		b += f[i];
-		b = (b << 1);
-	}
-	b += f[0];
-
-	//c = a * b;
-	cout << a << "\n" << c << " " << NumBits(a) << "\n" << b << "\n";
-
-	a = GenPrime_ZZ(512);
-	cout << a << "\n" << ProbPrime(a);*/
 }
